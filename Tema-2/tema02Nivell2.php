5a74b6e10d01fe17c5a424289aa68d1d22a9da4f
Rossana Liendo <rossana.liendo@gmail.com>
2024-04-11 10:15:00 +0200

    Finished Tema2Nivel2

--- Tema-2/tema02Nivell2.php
+++ Tema-2/tema02Nivell2.php
@@ -1,3 +1,39 @@
 <?php
 
+//EXERCICI 1
+
+$minutos = 0;
+
+function costoLlamada($minutos) {
+
+    $costo = 0;
+
+    if ($minutos > 0 && $minutos <= 3) {
+        $costo = 10;
+    } else {
+        $costo = 10 + (($minutos - 3) * 5);
+    }
+
+    return "Debe pagar " . $costo . " céntimos.";
+}
+
+echo costoLlamada(4);
+
+    // EXERCICI 2
+
+    $cantidadXocos = 0;
+    $cantidadXiclets = 0;
+    $cantidadCaramels = 0;
+
+    function totalCompra($cantidadXocos, $cantidadXiclets, $cantidadCaramels) {
+
+        $total = $cantidadXocos + ($cantidadXiclets * 0.5) + ($cantidadCaramels * 1.50);
+
+        return "El total de su compra es " . $total . " euros.\n";
+
+    }
+
+    echo totalCompra(2, 3, 4);
+
+
 ?>
