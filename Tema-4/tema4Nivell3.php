32c4717b344e0fd3f9fe1b4d21c64cc2e37b5096
Rossana Liendo <rossana.liendo@gmail.com>
2024-04-16 14:47:53 +0200

    Started Tema 4 Nivell 3

---
+++ Tema-4/tema4Nivell3.php
@@ -0,0 +1,15 @@
+<?php
+
+class Cinema {
+
+    //Atributos
+    private $nom;
+    private $poblacion;
+    private $listaPeliculas;
+
+    //Constructor
+    public function __construct(string $nom) {}
+
+}
+
+?>
