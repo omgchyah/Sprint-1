94779b9dd10de2549f864bcfc81eaa9a4a61c4bc
Rossana Liendo <rossana.liendo@gmail.com>
2024-04-18 11:59:04 +0200

    Created a session

--- Tema-6/tema6Nivell1.php
+++ Tema-6/tema6Nivell1.php
@@ -1,3 +1,16 @@
 <?php
+//Guardar datos mientras la sesión siga activa
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    //Recuperar los datos del formulario
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["nombre"] = $_POST["nombre"];
+    $_SESSION["apellido"] = $_POST["apellidos"];
+    $_SESSION["email"] = $_POST["email"];
+    $_SESSION["password"] = $_POST["password"];
+}
+
 
 ?>
