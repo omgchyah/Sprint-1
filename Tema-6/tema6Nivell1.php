82a1a382a0aefec888d8d7393b46a8aeacf3a0c7
Rossana Liendo <rossana.liendo@gmail.com>
2024-04-18 12:54:23 +0200

    Finished Tema6 Nivell1

--- Tema-6/tema6Nivell1.php
+++ Tema-6/tema6Nivell1.php
@@ -7,10 +7,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     //Recuperar los datos del formulario
     $_SESSION["username"] = $_POST["username"];
     $_SESSION["nombre"] = $_POST["nombre"];
-    $_SESSION["apellido"] = $_POST["apellidos"];
+    $_SESSION["apellidos"] = $_POST["apellidos"];
     $_SESSION["email"] = $_POST["email"];
     $_SESSION["password"] = $_POST["password"];
+
+    echo "<h2>Datos del formulario:<h2>";
+    echo "<p>Usuario: {$_SESSION["username"]}</p>";
+    echo "<p>Nombre: {$_SESSION["nombre"]}</p>";
+    echo "<p>Apellidos: {$_SESSION["apellidos"]}</p>";
+    echo "<p>E-mail: {$_SESSION["email"]}</p>";
+    echo "<p>Contraseña guardada con éxito.</p>";
 }
+
+//Verificar si guarda datos en la variable $_SESSION
+
+if(isset($_SESSION["username"])) {
+    $value = $_SESSION["username"];
+
+    //Mostrar variable
+    echo "Valor de 'username' es: " . $value ".";
+} else {
+    echo "La variable 'username' no se ha guardado.";
+}
+
 
 
 ?>
