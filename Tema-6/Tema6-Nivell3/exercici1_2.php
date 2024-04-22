3f7aea7e326c622fcbe6384b893504065f3caa2a
Rossana Liendo <rossana.liendo@gmail.com>
2024-04-22 14:48:30 +0200

    Started Tema 6 Nivell 3

---
+++ Tema-6/Tema6-Nivell3/exercici1_2.php
@@ -0,0 +1,21 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Carbon\Carbon;
+
+$now = Carbon::now();
+
+$myBirthday = Carbon::create(1988, 5, 15);
+
+if ($myBirthday->isBefore($now)) {
+    echo "Ya has cumplido años.";
+} else if ($myBirthday->isAfter($now)) {
+    $diasRestantes = $myBirthday->diffInDays($now);
+    echo "Cumples años en " . $diasRestantes . " días.";
+} else {
+    echo "¡Hoy cumples años!";
+}
+
+
+?>
