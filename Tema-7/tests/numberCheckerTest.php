6b0ef799e2e5b056d3dddd372b7e328eadcb4dda
Rossana Liendo <rossana.liendo@gmail.com>
2024-04-23 12:24:02 +0200

    Starting tests

---
+++ Tema-7/tests/numberCheckerTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+namespace Users\trait_exists
+
+use PHPUnit\Framework\TestCase;
+use Monicaestebanponce\Tema7\NumberChecker;
+
+class numberCheckerTest extends TestCase {
+    
+}
+
+?>
