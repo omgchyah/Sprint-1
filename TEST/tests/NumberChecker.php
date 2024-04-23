fc6fe93ac6fc1724cc4904616f241989a262381e
Rossana Liendo <rossana.liendo@gmail.com>
2024-04-23 14:40:31 +0200

    Created a TEST folder

---
+++ TEST/tests/NumberChecker.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+class NumberChecker  {
+
+
+	public function __construct(private int $number){}
+
+	public function isEven(): bool {
+		return $this->number%2 == 0;
+	}
+	public function isPositive(): bool {
+		return $this->number > 0;
+	}
+
+}
+?>
