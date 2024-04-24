32bb95d79093f6d6dd065f162468dfaedecc1351
Rossana Liendo <rossana.liendo@gmail.com>
2024-04-24 14:31:16 +0200

    Finished Tema 4 Nivell 2

--- Tema4/Nivell2/indexTema4Nivell2.php
+++ Tema4/Nivell2/indexTema4Nivell2.php
@@ -4,28 +4,52 @@ require "classPokerdice.php";
 
 $numDados = 0;
 
-function tirarDador(int $numDados): array {
+//Funcion para crear un array de dados
+function crearArrayDados(int $numDados): array {
 
-    $dadoX = new PokerDice();
-    $pokerDice = new PokerDice();
-
-    $arrayDados = [];
-    $arrayFigures = [];
+     $arrayDados = [];
 
     for($i = 0; $i < $numDados; $i++) {
         $arrayDados[] = $i;
     }
+
+    return $arrayDados;
+}
+
+//Funcion para obtener las figuras del array de dados
+function getFigures(array $arrayDados) {
+
+    $arrayFigures = [];
 
     foreach($arrayDados as $dado) {
         $figure = $dado->shapeName();
         $arrayFigures[] = $figure;
     }
 
-    $numTiradas = $pokerDice()
-
-
     return $arrayFigures;
-
 }
 
-?>
+
+//Crear Array de dados
+$fiveDados = crearArrayDados(5);
+
+//Obtener las figuras de los 5 dados
+$fiveFigures = getFigures($fiveDados);
+
+//Imprimir array de las figuras de los dados
+echo "Figuras de los " . $numDados . " dados:<br>";
+
+foreach($fiveFigures as $figure) {
+    echo $figure . "<br>";
+}
+
+
+//Llamar al metodo para obtener el numero total de tiradas del array de dados
+$pokerDice = new PokerDice();
+
+$numTiradas = $pokerDice->getTotalThrows($arrayDados);
+
+echo "El número total de tiradas es de " . $numTiradas;
+
+
+
